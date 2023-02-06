60ee5b7c35a80cd6f119d1ff6c42863946956925
Harsh_Dhameliya <harshdhameliya.dnk@gmail.com>
2023-02-06 18:06:41 +0530

    Update table layout.

--- app/src/main/java/com/harsh/roughdiamondrate/uiComponents/activity/RawCutHistoryActivity.kt
+++ app/src/main/java/com/harsh/roughdiamondrate/uiComponents/activity/RawCutHistoryActivity.kt
@@ -1,14 +1,22 @@
 package com.harsh.roughdiamondrate.uiComponents.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.harsh.roughdiamondrate.databinding.ActivityRawCutHistoryBinding
 
 class RawCutHistoryActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityRawCutHistoryBinding
+
+    private lateinit var binding: ActivityRawCutHistoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRawCutHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.enterDetails.setOnClickListener {
+            startActivity(Intent(this, RawCutDetailActivity::class.java))
+        }
     }
+
+
 }
